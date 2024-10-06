49051775ab95f258850a3fe5549d0af2dcdef927
Simon Coles <simonc@amphora-research.com>
2024-10-06 09:07:21 +0100

    Disable background threads for now

--- config/initializers/mqtt_tasks.rb
+++ config/initializers/mqtt_tasks.rb
@@ -3,19 +3,19 @@
 # Putting this in an after_initialize block so we can be sure Rails is loaded
 Rails.application.config.after_initialize do
   # Only run these tasks in production, we can do them in development manually
-  if Rails.env.production? || ENV["FORCE_THREADS"] == "YES"
-    # The listener thread
-    Thread.new do
-      Rails.application.executor.wrap do
-        MqttMessage.listen
-      end
-    end
+  # if Rails.env.production? || ENV["FORCE_THREADS"] == "YES"
+  #   # The listener thread
+  #   Thread.new do
+  #     Rails.application.executor.wrap do
+  #       MqttMessage.listen
+  #     end
+  #   end
 
-    # The pruning thread
-    Thread.new do
-      Rails.application.executor.wrap do
-        MqttMessage.prune_old
-      end
-    end
-  end
+  #   # The pruning thread
+  #   Thread.new do
+  #     Rails.application.executor.wrap do
+  #       MqttMessage.prune_old
+  #     end
+  #   end
+  # end
 end
