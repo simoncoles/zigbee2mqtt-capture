33a2a93f8f788d605052faaf8582202a312b50ca
Simon Coles <simonc@amphora-research.com>
2026-03-15 18:54:05 +0000

    Fix linting errors

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,9 @@ Rails.application.routes.draw do
   post "/monitoring/device/:id/recalculate", to: "monitoring#recalculate_threshold", as: :recalculate_threshold_monitoring
 
   # List screens
-  resources :mqtt_messages, only: [:index]
-  resources :devices, only: [:index]
-  resources :readings, only: [:index]
+  resources :mqtt_messages, only: [ :index ]
+  resources :devices, only: [ :index ]
+  resources :readings, only: [ :index ]
 
   # Defines the root path route ("/")
   root to: "dashboard#show"
